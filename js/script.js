4d0d8a6fea9ca961df103db0067d106308ddcb44
Wili Delgado <wilsondelgadomoreno@gmail.com>
2025-11-28 16:37:42 -0500

    generando script

--- js/script.js
+++ js/script.js
@@ -11,3 +11,16 @@ const observer = new IntersectionObserver(entries => {
 });
 
 logos.forEach(logo => observer.observe(logo));
+
+//vamos a crear interactividad al botom desde el doom
+
+const btn = document.querySelector(".btn-toggle");
+const card = document.querySelector(".blog-card-content");
+
+btn.addEventListener("click", () => {
+    card.classList.toggle("active");
+
+    btn.textContent = card.classList.contains("active")
+        ? "Leer menos"
+        : "Leer más";
+});
